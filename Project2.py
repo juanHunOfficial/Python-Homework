1bcf3f248b80c71aa0960ebfc406f8cb19e0ba68
juanHunOfficial <juanhun2424@gmail.com>
2024-04-04 10:37:35 -0400

    changing from volume formula to surface area

--- Project2.py
+++ Project2.py
@@ -1,3 +1,4 @@
 cube_edge_length = int(input("Enter the length of an edge for your cube: "))
-area = cube_edge_length * cube_edge_length * cube_edge_length
+NUMBER_OF_SIDES = 6
+area = NUMBER_OF_SIDES * cube_edge_length ** 2
 print("The surface area of the cube is: ", area)
