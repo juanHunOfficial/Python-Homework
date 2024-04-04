6e98a69db3f09422df483a5b10187bd732631787
juanHunOfficial <juanhun2424@gmail.com>
2024-04-04 12:54:58 -0400

    updating and refactoring calculations

--- solo_practice_code/calulations.py
+++ solo_practice_code/calulations.py
@@ -1,24 +1,48 @@
-def calculate_taxes_by_state(state_requested, starting_salary, home_value):
+def calculate_taxes_by_state(state_requested, starting_salary, net_income, home_value):
     state = ["Florida", "North Carolina", "Tennessee"]
-    categories = ["State Income Tax", "Sales Tax"]
     property_tax_by_state = [0.008, 0.007, 0.0067]
-    property_tax_by_state = property_tax_by_state[state_requested]
+    sales_tax_by_state = [0.06,0.0475, 0.07]
+    state_income_tax = [0, 0.0475, 0]
 
-    if state_requested == 1:
-        chosen_taxes_by_state = [0, 0.06]#Florida taxes in order according to categoires[]
-    elif state_requested == 2:
-        chosen_taxes_by_state = [0.0475, 0.0475]#North Carolina taxes in order according to categoires[]
-    elif state_requested == 3:
-        chosen_taxes_by_state = [0, 0.07]#Tennessee taxes in order according to categoires[]
-    else:
-        print("ERROR: Invalid response!")
-
+    state_income_tax = state_income_tax[state_requested - 1]
+    sales_tax_by_state = sales_tax_by_state[state_requested - 1]
+    property_tax_by_state = property_tax_by_state[state_requested - 1]
     state_to_display = state[state_requested - 1]
-    net_income = starting_salary
 
-    for index in range(len(categories)):
-        net_income = net_income - (starting_salary * chosen_taxes_by_state[index])
-        print(f"For the state of {state_to_display} your income after {categories[index]} is: ${round(net_income, 2)}")
+
+    print(f"For the state of {state_to_display} your income after the state income tax is: ${round(net_income, 2)}")
 
     net_income = net_income - (home_value * property_tax_by_state)
-    print(f"Your net income after your annual property taxes is: {net_income}")
+    print("Note: Sales tax will be changed in future iterations to be more accurate,"
+          "for now it will be calculated and removed from the starting salary.")
+    print(f"Your net income after your annual property taxes is: ${net_income}")
+
+    return net_income
+
+def calculate_federal_income_tax(starting_salary):
+    #This will be for married filing jointly 2023 taxes
+    federal_income_range = [0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37]
+    federal_income_limit = [11_000, 44_725, 95_375, 182_100, 231_250, 346_875, 346_876]
+
+    salary_index = 0
+    if starting_salary < federal_income_limit[0]:
+        salary_index = 0
+    elif starting_salary < federal_income_limit[1]:
+        salary_index = 1
+    elif starting_salary < federal_income_limit[2]:
+        salary_index = 2
+    elif starting_salary < federal_income_limit[3]:
+        salary_index = 3
+    elif starting_salary < federal_income_limit[4]:
+        salary_index = 4
+    elif starting_salary < federal_income_limit[5]:
+        salary_index = 5
+    elif starting_salary >= federal_income_limit[6]:
+        salary_index = 6
+
+    net_income = starting_salary
+    net_income = net_income - (starting_salary * federal_income_range[salary_index])
+
+    print(f"Your net income after federal taxes have been removed is: ${net_income}")
+
+    return net_income
--- solo_practice_code/cost_of_living_calc.py
+++ solo_practice_code/cost_of_living_calc.py
@@ -1,4 +1,4 @@
-from calulations import calculate_taxes_by_state
+from calulations import calculate_taxes_by_state, calculate_federal_income_tax
 
 state_requested = int(input("Select the state you wish to check from the menu:"
                         "\n 1) Florida"
@@ -10,4 +10,6 @@ print(state_requested)
 starting_salary = float(input("Enter your starting salary: "))
 home_value = float(input("Enter in your desired home value: "))
 
-calculate_taxes_by_state(state_requested, starting_salary, home_value)
+
+net_income = calculate_federal_income_tax(starting_salary)
+calculate_taxes_by_state(state_requested, starting_salary, net_income, home_value)
