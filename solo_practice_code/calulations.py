86090b20ce0ea0c2cee6a5f996b3a43053c3f8aa
juanHunOfficial <juanhun2424@gmail.com>
2024-04-07 09:16:38 -0400

    added cost of living stats

--- solo_practice_code/calulations.py
+++ solo_practice_code/calulations.py
@@ -12,9 +12,10 @@ def calculate_taxes_by_state(state_requested, starting_salary, net_income, home_
     net_income = net_income - (home_value * property_tax_by_state)
     print(f"Your net income after your annual property taxes is: ${net_income}")
     net_income = net_income - (starting_salary * state_income_tax)
-    print(f"For the state of {state_to_display} your income after the state income tax is: ${round(net_income, 2)}")
+    print(f"For the state of {state_to_display} your income after the state income tax is: "
+          f"${round(net_income, 2)}")
 
-    print(f"Living in this state will cost you: ${(starting_salary - net_income)}")
+    print(f"Living in this state will cost you: ${(starting_salary - net_income)} per year")
     return net_income
 
 def calculate_federal_income_tax(starting_salary):
@@ -42,7 +43,25 @@ def calculate_federal_income_tax(starting_salary):
     net_income = starting_salary
     net_income = net_income - (starting_salary * federal_income_range[salary_index])
 
-    print(f"Based on your income for married filing jointly, you fall under the {(federal_income_range[salary_index] * 100)}% bracket")
+    print(f"Based on your income for married filing jointly, you fall under the "
+          f"{(federal_income_range[salary_index] * 100)}% bracket")
     print(f"Your net income after federal taxes have been removed is: ${net_income}")
 
     return net_income
+
+def display_cost_of_living_stats(state_requested, net_income):
+    city_of_state = ["Tampa", "Raleigh", "Nashville"]
+    average_col_family_of_4 = [4_759.6, 4_550.5, 4_449.6]#fl(tampa), nc(raleigh), tn(nashville)
+    average_col_single = [1_339.5, 1_290.2, 1_234.5]#fl(tampa), nc(raleigh), tn(nashville)
+    state_requested -= 1
+
+    print(f"The average cost of living in {city_of_state[state_requested]} is ${average_col_family_of_4[state_requested]} for "
+          f"a family of four, and ${average_col_single[state_requested]} for a single person.")
+    NUMBER_OF_MONTHS = 12
+    estimated_monthly_income = round((net_income / NUMBER_OF_MONTHS), 2)
+    estimated_monthly_surplus_fam4 = estimated_monthly_income - average_col_family_of_4[state_requested]
+    estimated_monthly_surplus_single = estimated_monthly_income - average_col_single[state_requested]
+
+    print(f"Based on your net income of ${net_income} you will have about ${estimated_monthly_income} "
+          f"with an estimated ${estimated_monthly_surplus_fam4} in surplus per month for a family of 4, and "
+          f"${estimated_monthly_surplus_single} for a single person")
--- solo_practice_code/cost_of_living_calc.py
+++ solo_practice_code/cost_of_living_calc.py
@@ -1,4 +1,4 @@
-from calulations import calculate_taxes_by_state, calculate_federal_income_tax
+from calulations import calculate_taxes_by_state, calculate_federal_income_tax, display_cost_of_living_stats
 
 state_requested = int(input("Select the state you wish to check from the menu:"
                         "\n 1) Florida"
@@ -13,3 +13,4 @@ home_value = float(input("Enter in your desired home value: "))
 
 net_income = calculate_federal_income_tax(starting_salary)
 calculate_taxes_by_state(state_requested, starting_salary, net_income, home_value)
+display_cost_of_living_stats(state_requested, net_income)
