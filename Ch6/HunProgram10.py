a70b753dd822cf1af9b95812e1c95de941c3be12
juanHunOfficial <juanhun2424@gmail.com>
2024-04-24 11:29:29 -0400

    refactored the calculations for the state income, property tax, and federal tax

--- Ch6/HunProgram10.py
+++ Ch6/HunProgram10.py
@@ -1,2 +1,21 @@
-def my_range():
-    pass
+def my_range(start, stop = None, step = 1):
+    if stop is None:
+        stop = start
+        start = 0
+
+    current = start
+    lyst = []
+    while (step > 0 and current < stop) or (step < 0 and current > stop):
+        lyst.append(current)
+        current += step
+    return lyst
+
+for x in my_range(5):
+    print(x)
+
+for x in range(5):
+    print(x)
+
+print(my_range(5))
+print(range(5))# i found that it says to retrun a range function but im not
+               #sure what that means. Would i have to define an entire range class?
